58eae81ea5fbe67da3c9977993413d459d31beb7
LonelyVikingMichael <michael@lonelyviking.com>
2023-10-23 21:32:34 +0200

    [test] add missing ids

--- tests/test_flattening.py
+++ tests/test_flattening.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-
+from unittest.mock import ANY
 from uuid import UUID
 
 from src.main import flatten_model_instance
@@ -16,8 +16,8 @@ def test_flatten_model_instance(supplier: Supplier) -> None:
                 "created_at": "2020-02-21 00:00:00",
                 "email": "info@loros.example",
                 "name": "Loros Grist",
-                "address_id": None,
-                "bank_details_id": None,
+                "address_id": "c5fb851f-63fd-4572-872c-3597186c9afe",
+                "bank_details_id": "ccd390cf-a74c-4897-a923-3d77ce1b97bf",
                 "id": "2b7e7211-d2c7-4eb4-8c14-05ed58c77473",
             }
         ],
@@ -41,22 +41,22 @@ def test_flatten_model_instance(supplier: Supplier) -> None:
         ],
         SupplierCategory.__table__: [
             {
-                "supplier_id": UUID("2b7e7211-d2c7-4eb4-8c14-05ed58c77473"),
-                "category_id": UUID("3674c73c-a967-493f-9a4b-5b70f78a5a99"),
-                "id": UUID("764e903c-3b56-4739-bbad-1f0bbfd60a7e"),
+                "supplier_id": "2b7e7211-d2c7-4eb4-8c14-05ed58c77473",
+                "category_id": "3674c73c-a967-493f-9a4b-5b70f78a5a99",
+                "id": ANY,
             },
             {
-                "supplier_id": UUID("2b7e7211-d2c7-4eb4-8c14-05ed58c77473"),
-                "category_id": UUID("f66c3eb7-7b93-4d9f-bc66-8ff07353f5e7"),
-                "id": UUID("747e7a96-3493-449a-8e65-9c3adc2d5089"),
+                "supplier_id": "2b7e7211-d2c7-4eb4-8c14-05ed58c77473",
+                "category_id": "f66c3eb7-7b93-4d9f-bc66-8ff07353f5e7",
+                "id": ANY,
             },
         ],
         Contact.__table__: [
             {
                 "name": "Sveimann Glort",
                 "email": "sveimann@loros.example",
-                "address_id": None,
-                "supplier_id": None,
+                "address_id": "cd521f7e-df61-4079-b44d-35015b9b5110",
+                "supplier_id": "2b7e7211-d2c7-4eb4-8c14-05ed58c77473",
                 "id": "98a11210-949a-48ad-99c7-1d89c54c2a53",
             }
         ],
