0f22200c5bb293f88e2a70ef6fd353f8bed8f48f
LonelyVikingMichael <michael@lonelyviking.com>
2023-10-26 09:41:02 +0200

    [fix] handle non list like collections

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from typing import Any
 from uuid import UUID, uuid4
 
 from sqlalchemy import Table, inspect
-from sqlalchemy.orm import DeclarativeBase, Relationship
+from sqlalchemy.orm import DeclarativeBase, KeyFuncDict, Relationship
 
 # TODO: change to configuration or function arguments
 INSERT_SECONDARY_ID = True
@@ -32,10 +32,13 @@ def flatten_model_instance(
         if relationship.back_populates:
             if (combination := {model.__tablename__, relationship.target.name}) in back_populates_tracker:
                 continue
-            else:
-                back_populates_tracker.append(combination)
+
+            back_populates_tracker.append(combination)
         if relationship.uselist:
-            for child in getattr(model, relationship.key):
+            collection = getattr(model, relationship.key)
+            if isinstance(collection, KeyFuncDict):
+                collection = collection.values()
+            for child in collection:
                 # recursive flattening
                 data_map = flatten_model_instance(child, data_map)
 
@@ -56,18 +59,20 @@ def _append_mapping(data_map: dict[Table, list[dict[str, Any]]], table: Table, d
     return data_map
 
 
-def generate_secondary_row(relationship: Relationship, parent: DeclarativeBase, child: DeclarativeBase) -> dict[str, Any]:
+def generate_secondary_row(
+    relationship: Relationship, parent: DeclarativeBase, child: DeclarativeBase
+) -> dict[str, Any]:
     """Generate rows for secondary a.k.a. association tables."""
     secondary_dict = {}
     for column in relationship.remote_side:
         foreign_key = next(iter(column.foreign_keys))
-        secondary_dict[column.key] = (
-            getattr(parent, foreign_key.column.key)
+        secondary_dict[column.name] = (
+            getattr(parent, foreign_key.column.name)
             if foreign_key.column.table == parent.__table__
-            else getattr(child, foreign_key.column.key)
+            else getattr(child, foreign_key.column.name)
         )
-        if STRINGIFY_UUIDS and isinstance(secondary_dict[column.key], UUID):
-            secondary_dict[column.key] = str(secondary_dict[column.key])
+        if STRINGIFY_UUIDS and isinstance(secondary_dict[column.name], UUID):
+            secondary_dict[column.name] = str(secondary_dict[column.name])
     if INSERT_SECONDARY_ID:
         secondary_dict[SECONDARY_ID_COLUMN_NAME] = str(uuid4()) if STRINGIFY_UUIDS else uuid4()
 
@@ -93,7 +98,7 @@ def deduplicate_data_mapping(data_map: dict[Table, list[dict[str, Any]]]) -> dic
     return {table: [dict(t) for t in {tuple(d.items()) for d in values}] for table, values in data_map.items()}
 
 
-def write_mapping(data: dict[Table, list[dict[str, Any]]], path: str) -> None:
+def write_raw(data: dict[Table, list[dict[str, Any]]], path: str) -> None:
     """Write a data mapping to a file."""
 
     with open(path, "w") as file:
@@ -101,9 +106,32 @@ def write_mapping(data: dict[Table, list[dict[str, Any]]], path: str) -> None:
             file.write(f"{table.name} = {value_list}\n")
 
 
-def model_columns_to_dict(
-    model: DeclarativeBase
-) -> dict[str, Any]:
+def write_sql(data: dict[Table, list[dict[str, Any]]], path: str) -> None:
+    """Write a datamapping as raw SQL `INSERT` statements."""
+
+    with open(path, "w") as file:
+        for table, data_list in data.items():
+            value_list = []
+            for data_map in data_list:
+                values = []
+                for item in data_map.values():
+                    if item is None:
+                        value = "NULL"
+                    elif isinstance(item, str):
+                        value = f"""'{item.replace("'", "''")}'"""
+                    elif isinstance(item, date | bool):
+                        value = f"'{item}'"
+                    else:
+                        value = str(item)
+                    values.append(value)
+                value_list.append(f'    ({", ".join(values)})')
+            value_string = ",\n".join(value_list)
+            file.write(
+                f"""\nINSERT INTO "{table.name}" ({", ".join(data_list[0].keys())})\nVALUES\n{value_string};\n"""
+            )
+
+
+def model_columns_to_dict(model: DeclarativeBase) -> dict[str, Any]:
     """Convert a SQLAlchemy model to a dictionary."""
 
     inspector = inspect(model)
@@ -112,15 +140,15 @@ def model_columns_to_dict(
     for column in inspector.mapper.column_attrs:
         value = getattr(model, column.key)
         if USE_ENUM_VALUES and isinstance(value, Enum):
-            mapping[column.key] = value.value
+            mapping[column.expression.key] = value.value
             continue
         if STRINGIFY_DATES and isinstance(value, date):
-            mapping[column.key] = str(value)
+            mapping[column.expression.key] = str(value)
             continue
         if STRINGIFY_UUIDS and isinstance(value, UUID):
-            mapping[column.key] = str(value)
+            mapping[column.expression.key] = str(value)
             continue
 
-        mapping[column.key] = value
+        mapping[column.expression.key] = value
 
     return mapping
