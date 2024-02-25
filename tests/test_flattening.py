cbaed18609746e8b21d2b03aac8343f5e72ab960
LonelyVikingMichael <michael@lonelyviking.com>
2024-02-25 18:06:58 +0200

    test: check collection results

--- tests/test_flattening.py
+++ tests/test_flattening.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from unittest.mock import ANY
 
-from sqlalchemy_flattener import flatten
 from examples.models import (
     Address,
     BankDetails,
@@ -11,10 +10,11 @@ from examples.models import (
     Supplier,
     SupplierCategory,
 )
+from sqlalchemy_flattener import flatten
 
 
-def test_flatten_model_instance(supplier: Supplier) -> None:
-    data = flatten(supplier)
+def test_flatten_model_instance(suppliers: Supplier) -> None:
+    data = flatten(suppliers)
 
     assert data[Supplier.__table__] == [
         {
@@ -24,9 +24,17 @@ def test_flatten_model_instance(supplier: Supplier) -> None:
             "address_id": "c5fb851f-63fd-4572-872c-3597186c9afe",
             "bank_details_id": "ccd390cf-a74c-4897-a923-3d77ce1b97bf",
             "id": "2b7e7211-d2c7-4eb4-8c14-05ed58c77473",
-        }
+        },
+        {
+            "created_at": "2020-02-21 00:00:00",
+            "email": "info@yvon.example",
+            "name": "Salvoss Yvon",
+            "address_id": "c307f778-1888-4077-9888-55bce7738738",
+            "bank_details_id": "0cb8bffb-7323-46de-88e0-3a9f3a7edfcf",
+            "id": "8f4f00f7-3352-4579-84ef-e2f8a455afc3",
+        },
     ]
-    assert len(data[Address.__table__]) == 2
+    assert len(data[Address.__table__]) == 4
     assert all(
         d in data[Address.__table__]
         for d in [
@@ -35,6 +43,11 @@ def test_flatten_model_instance(supplier: Supplier) -> None:
                 "line_1": "The imperial road",
                 "id": "cd521f7e-df61-4079-b44d-35015b9b5110",
             },
+            {"id": "c307f778-1888-4077-9888-55bce7738738", "line_1": "Hovallii Psot"},
+            {
+                "id": "b94ed931-45a9-4a9a-8456-a22efe797fc2",
+                "line_1": "Another departure",
+            },
         ]
     )
     assert data[BankDetails.__table__] == [
@@ -42,7 +55,12 @@ def test_flatten_model_instance(supplier: Supplier) -> None:
             "account_number": "payusnothing",
             "account_type": "cash",
             "id": "ccd390cf-a74c-4897-a923-3d77ce1b97bf",
-        }
+        },
+        {
+            "account_number": "payless",
+            "account_type": "cash",
+            "id": "0cb8bffb-7323-46de-88e0-3a9f3a7edfcf",
+        },
     ]
     assert len(data[Category.__table__]) == 2
     assert all(
@@ -52,7 +70,7 @@ def test_flatten_model_instance(supplier: Supplier) -> None:
             {"name": "ISP", "id": "f66c3eb7-7b93-4d9f-bc66-8ff07353f5e7"},
         ]
     )
-    assert len(data[SupplierCategory.__table__]) == 2
+    assert len(data[SupplierCategory.__table__]) == 4
     assert all(
         d in data[SupplierCategory.__table__]
         for d in [
@@ -66,6 +84,16 @@ def test_flatten_model_instance(supplier: Supplier) -> None:
                 "category_id": "f66c3eb7-7b93-4d9f-bc66-8ff07353f5e7",
                 "id": ANY,
             },
+            {
+                "supplier_id": "8f4f00f7-3352-4579-84ef-e2f8a455afc3",
+                "category_id": "3674c73c-a967-493f-9a4b-5b70f78a5a99",
+                "id": ANY,
+            },
+            {
+                "supplier_id": "8f4f00f7-3352-4579-84ef-e2f8a455afc3",
+                "category_id": "f66c3eb7-7b93-4d9f-bc66-8ff07353f5e7",
+                "id": ANY,
+            },
         ]
     )
     assert data[Contact.__table__] == [
@@ -75,7 +103,14 @@ def test_flatten_model_instance(supplier: Supplier) -> None:
             "address_id": "cd521f7e-df61-4079-b44d-35015b9b5110",
             "supplier_id": "2b7e7211-d2c7-4eb4-8c14-05ed58c77473",
             "id": "98a11210-949a-48ad-99c7-1d89c54c2a53",
-        }
+        },
+        {
+            "name": "Jevutte Nosk",
+            "email": "jevutte@yvon.example",
+            "address_id": "b94ed931-45a9-4a9a-8456-a22efe797fc2",
+            "supplier_id": "8f4f00f7-3352-4579-84ef-e2f8a455afc3",
+            "id": "97e9593f-4f15-40c2-932f-a0aa5b500f8b",
+        },
     ]
 
 
